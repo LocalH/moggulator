e600ff49ddb0ecfccc4073037756e64369bc33ee
LocalH <localhbci@gmail.com>
2024-02-12 20:45:56 -0500

    v11 key, and v12-17 HvKeys added and selected based on input file

--- moggulator.py
+++ moggulator.py
@@ -7,18 +7,18 @@ import os
 
 def usage():
    #print(f'usage: {sys.argv[0]} [-h] [-e] [-i <input>] [-o <output>] [-v]')
-   print(f'usage: {os.path.relpath(sys.argv[0])} [-h] [-i <input>] [-o <output>] [-v <log>]')
+   print(f'usage: {os.path.relpath(sys.argv[0])} [-h] [-i <input>] [-o <output>] [-v]')
    print("   -h / --help : print usage")
    #print("   -e / --encrypt: encrypt mogg")
    print("   -i <input> / --input=<input> : read mogg from <input>")
    print("   -o <output> / --output=<output> : write mogg to <output>")
-   print("   -v <log> / --verbose=<log>: verbose output to <log>\n")
+   print("   -v / --verbose: verbose output to stderr\n")
    print("Default with no arguments is to read from stdin and write to stdout.")
    print("Default action is to decrypt. Encryption coming in the future.")
 
 def main():
    try:
-      opts, args = getopt.getopt(sys.argv[1:], "hei:o:v:", ["help", "encrypt", "input=", "output=", "verbose="])
+      opts, args = getopt.getopt(sys.argv[1:], "hei:o:v", ["help", "encrypt", "input=", "output=", "verbose"])
    except getopt.GetoptError as err:
       print(err)
       usage()
@@ -38,8 +38,6 @@ def main():
       if o in ("-v", "--verbose"):
          no_opts = False
          verbose = True
-         if a:
-            logfile = a
       elif o in ("-h", "--help"):
          no_opts = False
          usage()
@@ -74,10 +72,8 @@ def main():
       fin = open(infile, 'rb')
    if outfile:
       fout = open(outfile, 'wb')
-   if logfile:
-      flog = open(logfile, 'w')
 
-   mogglib.decrypt_mogg(fin, fout, flog)
+   mogglib.decrypt_mogg(fin, fout, flog, verbose)
    
    print("Finished.")
 
